92e88b828421e8a46cee73672fa2005ad81d999b
Chisanuphong Nujtes <chinujte@z2t4c4.42bangkok.com>
2024-06-05 17:57:02 +0700

    Update

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -3,102 +3,80 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cnzk <cnzk@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: chinujte <chinujte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 01:41:15 by cnzk              #+#    #+#             */
-/*   Updated: 2024/06/05 04:23:19 by cnzk             ###   ########.fr       */
+/*   Updated: 2024/06/05 17:56:37 by chinujte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 /*
-	1 arbitaary
-	2 numeric
-	3 get len 
-	4 pow
-	5 ft_atoi
+- ignore space
+- get sign
+- after sign shouldn't space
+- get pos of start num && end num
+- turn num to int
+- int num * sign
 */
 
-int is_arbitrary(char str, int *arbitrary)
+int	find_sign(char *str, int *sign, int *index)
 {
-	if (str == '-')
-		*arbitrary *= -1;    
-	return (str == ' ' || str == '-' || str == '+');
+	int	i;
+
+	i = 0;
+	*sign = 1;
+	while (str[i] == ' ' || str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			*sign *= -1;
+		if ((str[i] == '-' || str[i] == '+') && str[i + 1] == ' ')
+			return (0);
+		i++;
+	}
+	*index = i;
+	return (1);
 }
 
-int is_numeric(char str)
+int	is_numeric(char c)
 {
-	return ('0' <= str && str <= '9');
+	return ('0' <= c && c <= '9');
 }
 
-int str_len(char *str)
+int	ft_atoi(char *str)
 {
-	int len;
-
-	len = 0;
-	while (str[len])
-		len++;
-	return (len);
-}
-
-int	turn_to_int(char *str)
-{
+	int	sign;
+	int	num_index;
 	int	number;
 	int	pow;
-	int	len;
-	int	i;
-	int	j;
 
 	number = 0;
-	len = str_len(str) - 1;
-	i = 0;
-	while (str[i])
+	pow = 0;
+	if (find_sign(str, &sign, &num_index))
 	{
-		j = 0;
-		pow = 1;
-		while (j < len - i)
+		while (str[num_index])
 		{
-			pow *= 10;
-			j++;
+			if (is_numeric(str[num_index]))
+			{
+				number *= pow * 10;
+				number += str[num_index] - 48;
+			}
+			else
+				return (number * sign);
+			pow++;
+			num_index++;
 		}
-		number += (str[i] - '0') * pow;
-		i++;
 	}
-	return (number);
+	return (0);
 }
-
-int ft_atoi(char *str)
-{
-	int		i;
-	int		j;
-	int		arbitrary;
-
-	i = 0;
-	j = 0;
-	arbitrary = 1;
-	while (str[i])
-	{
-		if (is_numeric(str[i]))
-		{
-			j++;
-		}	        
-		else if (!(is_arbitrary(str[i], &arbitrary)))
-		{
-			nbr[j] = '\0';
-			return (turn_to_int(nbr) * arbitrary);
-		}	
-		i++;
-	}
-	nbr[j] = '\0';
-	return (turn_to_int(nbr) * arbitrary);
-}
-
+/*
 #include <stdio.h>
+
 int main(int argc, char **argv)
 {
 	if (argc != 2)
-		return 1;
-
-	int x = ft_atoi(argv[1]);
-	printf("%d", x);
+		return (0);
+	printf("%d", ft_atoi(argv[1]));
 	return 0;
 }
+*/
