0325a50ba0607542ceb665d0049394b9327ae11a
Chisanuphong Nujtes <chinujte@z2t4c4.42bangkok.com>
2024-06-08 12:56:40 +0700

    Update

--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: chinujte <chinujte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 18:14:03 by chinujte          #+#    #+#             */
-/*   Updated: 2024/06/05 21:44:35 by chinujte         ###   ########.fr       */
+/*   Updated: 2024/06/07 17:21:30 by chinujte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,19 @@ int	base_check(char *base, int *base_number)
 {
 	char	*hex_base;
 	int		i;
-
-	hex_base = "0123456789ABCDEF";
+	int		j;
+	
 	i = 0;
+	j = 0;
 	*base_number = 0;
 	while (base[i])
 	{
-		if (base[i] != hex_base[i])
-			return (0);	
+		j = i;
+		while (base[++j])
+		{
+			if (base[i] == base[j])
+				return (0);
+		}
 		i++;
 	}
 	*base_number = i;
@@ -54,7 +59,7 @@ void	turn_to_base(int nbr, int base, char *base_code)
 void	ft_putnbr_base(int nbr, char *base)
 {
 	int		base_number;
-	
+
 	if (base_check(base, &base_number))
 	{
 		turn_to_base(nbr, base_number, base);
@@ -63,6 +68,11 @@ void	ft_putnbr_base(int nbr, char *base)
 
 int	main(void)
 {
-	ft_putnbr_base(20, "01");
-	return (0);
+	// ft_putnbr_base(-12, "01");
+	// printf("\n");
+	// ft_putnbr_base(-40, "poneyvif");
+	// printf("\n");
+	// ft_putnbr_base(894867, "+0123456789");
+	// printf("\n");
+	ft_putnbr_base(12, "0123456789ABCDEF");
 }
