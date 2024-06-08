2b045883da4ff90ddbc1506c7c8dce9a2658a20a
Chisanuphong Nujtes <chinujte@z2t4c4.42bangkok.com>
2024-06-08 21:42:59 +0700

    auto push

--- ACTIVE_PROJECT/FINSH/C05/ex06/ft_is_prime.c
+++ ACTIVE_PROJECT/FINSH/C05/ex06/ft_is_prime.c
@@ -6,7 +6,7 @@
 /*   By: chinujte <chinujte@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 13:58:24 by chinujte          #+#    #+#             */
-/*   Updated: 2024/06/08 21:10:30 by chinujte         ###   ########.fr       */
+/*   Updated: 2024/06/08 21:42:32 by chinujte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,32 @@ int	ft_sqrt(int nb)
 	return (result);
 }
 
+int	ft_sqrt(int nb)
+{
+	long	result;
+
+	result = 0;
+	while (nb != result * result)
+	{
+		if (nb < result * result)
+			return (result - 1);
+		result++;
+	}
+	return (result);
+}
+
 int	ft_is_prime(int nb)
 {
-	int	basic_prime[7];
-	int	check;
-	int	i;
+	int	root;
 
-	if (nb < 2)
+	root = ft_sqrt(nb);
+	if (root != 0)
 		return (0);
-	get_basic_prime(basic_prime);
-	i = -1;
-	check = 0;
-	while (basic_prime[++i])
+	
+	while ()
 	{
-		if (nb == basic_prime[i])
-			return (1);
-		if (nb % basic_prime[i] != 0)
-			check++;
+		/* code */
 	}
-	return (check == 6);
+	
+	return (0);
 }
